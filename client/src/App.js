89af68888305763aa17d0442bddcdced9f6e81df
fpr <frankpiercereynolds95@gmail.com>
2022-11-14 18:29:39 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<SearchBooks />} />
+          <Route path="/saved" element={<SavedBooks />} />
+          <Route
+            path="*"
+            element={<h1 className="display-2">Wrong page!</h1>}
+          />
+        </Routes>
+      </>
+    </Router>
+  );
+}
+
+export default App;
