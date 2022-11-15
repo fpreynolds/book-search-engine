e29df28eb55c06cd24b06fc1a0614d7587063040
fpr <frankpiercereynolds95@gmail.com>
2022-11-14 22:43:53 -0500

    one more for good measure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ApolloProvider } from "@apollo/react-hooks";
-import ApolloClient from "apollo-boost";
+import { ApolloClient, ApolloProvider } from "@apollo/client";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,9 +2,9 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 
-import { LOGIN_USER } from "@apollo/react-hooks";
+import { LOGIN_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
-import { useMutation } from "../utils/mutations";
+import { useMutation } from "@apollo/react-hooks";
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: "", password: "" });
